dffe12c31f5f5b5cd8d1d91e97f45c732c82e86e
Kellylaporsh <104526215+Kellylaporsh@users.noreply.github.com>
2023-11-02 00:51:15 -0700

    Add files via upload

---
+++ simplecalculator/script.js
@@ -0,0 +1,19 @@
+let count = 0
+
+let result = document.getElementById("showresult")
+
+function zero(){
+    result.innerText = count = 0
+}
+
+function three(){
+    result.innerText = count += 3
+}
+
+function nine(){
+    result.innerText = count += 9
+}
+
+function minustwo(){
+    result.innerText = count -= 2
+}
